11315016a153b99f33420d974326bc2366618a73
Gaetan <gaetan@walloftraders.com>
2026-06-20 12:05:13 +0200

    auto: CrChantierPage.tsx"

--- app/src/cr-chantier/CrChantierPage.tsx
+++ app/src/cr-chantier/CrChantierPage.tsx
@@ -271,7 +271,7 @@ export default function CrChantierPage() {
       await saveCr({
         projet,
         dateReunion: crData.date_reunion || undefined,
-        crData: crData as object,
+        crData: crData as unknown as Record<string, unknown>,
       });
       setIsSaved(true);
       refetchHistory();
