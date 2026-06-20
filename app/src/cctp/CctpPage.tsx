c6e235cb275c0f0e477aac4ddfbc50b2d73354a6
Gaetan <gaetan@walloftraders.com>
2026-06-20 10:57:02 +0200

    auto: CctpPage.tsx"

--- app/src/cctp/CctpPage.tsx
+++ app/src/cctp/CctpPage.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { Button } from '../client/components/ui/button';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../client/components/ui/card';
 import { Input } from '../client/components/ui/input';
