06b4bc606596f06e0c01d0803e233ef5fb9c5107
Gaetan <gaetan@walloftraders.com>
2026-06-25 23:31:11 +0200

    auto: constants.ts"

--- app/src/client/components/NavBar/constants.ts
+++ app/src/client/components/NavBar/constants.ts
@@ -11,4 +11,5 @@ export const demoNavigationitems: NavigationItem[] = [
   { name: "CR Chantier", to: routes.CrChantierRoute.to },
   { name: "CCTP", to: routes.CctpRoute.to },
   { name: "Assistant PLU", to: routes.PluRoute.to },
+  { name: "Paramètres", to: routes.CabinetSettingsRoute.to },
 ] as const;
